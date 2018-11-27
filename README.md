533c941892c902071c4120bed33b808d52c181f8
pgarba <p@g>
2018-11-27 12:13:38 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
 # Hulk
+
 Hulk is able to bruteforce missing bytes after a DCA attack on AES with his special ability of AES-NI
+
+Hulk makes use of the AES-NI extension to bruteforce up to 6 missing bytes of AES. If you are lucky to own
+a Threadrippe 1950x, Hulk will use 2 AES-NI engines per core which makes the attack really fast.
+
+
+# Numbers
+
+Threadripper 1950x (2 AES NI Units per Core)
+32 bit - aes ni 32 threads : 1.2s
+40 bit - aes ni 32 threads : 5.17m
+48 bit - aes ni 32 threads : ~23h
+
+Core i7-6700k @ 4ghz (1 AES NI Unit per Core)
+32 bit - aes ni 4 threads : real 0m20.776s
+32 bit - aes ni 8 threads : real 0m29.173
+
+
+# Installation
+
+Compile and test with the given script ./build_and_test.sh or by using clang:
+
+clang++-7 aes.cpp -O3 -march=native -fms-extensions -o hulk -lpthread -g
+
+
+# Usage
+
+./hulk <e|d> <input> <output> <key> <10:Optional>
+
+<e|d>         - Attack (e) encryption or (d) decryption
+<input>       - The to input to the AES whitebox
+<output>      - The output of the AES whitebox
+<key>         - The partial key retrieved by the DCA attack
+                Mark the missing bytes with '??'
+<10:Optional> - Handle the key as a round 10 key
+                Hulk will calculate the round 0 key and test it against the given test vector 
+
+
+# Example
+
+time ./hulk d 00000000000000000000000000000000 ac589cfe8a7ae5dd875d9786e5832400 462f8e8e6ec0fe2b1d63????9f7f???? 10
+
+AES Key tester v0.1 (pgarba 2018)
+[*] AES-NI is supported by this CPU!
+[*] Round           : 10
+[*] Mode            : Decryption
+[*] Key             : 462f8e8e6ec0fe2b1d6300009f7f0000
+[*] Input           : 00000000000000000000000000000000
+[*] Expected        : ac589cfe8a7ae5dd875d9786e5832400
+[!] Bruteforce      : 4 missing bytes
+[*] Byte 0 Index 10
+[*] Byte 1 Index 11
+[*] Byte 2 Index 14
+[*] Byte 3 Index 15
+[*] AES-NI Units    : 8
+[*] Range           : 00000000 - FFFFFFFF
+[*] Step            : 1FFFFFFF
+[*] T00 Range       : 00000000 - 1FFFFFFF
+[*] T01 Range       : 20000000 - 3FFFFFFF
+[*] T02 Range       : 40000000 - 5FFFFFFF
+[*] T03 Range       : 60000000 - 7FFFFFFF
+[*] T04 Range       : 80000000 - 9FFFFFFF
+[*] T05 Range       : A0000000 - BFFFFFFF
+[*] T06 Range       : C0000000 - DFFFFFFF
+[*] T07 Range       : E0000000 - FFFFFFFF
+[!] T06 Key found   : 3525ac10bb391d8d5f3914fe94341985
+[*] Output          : ac589cfe8a7ae5dd875d9786e5832400
+[!] Valid key!
+
+
+real    0m29.173s
+user    3m49.406s
+sys     0m0.141s
---
+++ aes.cpp
@@ -0,0 +1,354 @@
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+#include <vector>
+#include <cmath>
+#include <thread>
+
+// aes ni
+#include "aesni.h"
+
+// key schedule to get round 0 key from round 10 key
+#include "keyschedule.h"
+
+typedef struct {
+  int ThreadID;
+  uint64_t Min;
+  uint64_t Max;
+} Range;
+static std::vector<Range> Ranges;
+
+typedef struct {
+  int Index;
+  uint8_t Value;
+  int Shift;
+} BByte;
+static std::vector<BByte> MissingBytes;
+
+
+static uint8_t *CHRHEX = (uint8_t *)
+    "\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF" \
+    "\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF" \
+    "\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF" \
+    "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\xFF\xFF\xFF\xFF\xFF\xFF" \
+    "\xFF\x0A\x0B\x0C\x0D\x0E\x0F\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF" \
+    "\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF" \
+    "\xFF\x0A\x0B\x0C\x0D\x0E\x0F\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF" \
+    "\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF" \
+    "\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF" \
+    "\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF" \
+    "\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF" \
+    "\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF" \
+    "\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF" \
+    "\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF" \
+    "\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF" \
+    "\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF";
+ 
+
+static bool GetNNICapability()
+{
+    unsigned int b;
+
+    __asm
+    {
+        mov     eax, 1
+        cpuid
+        mov     b, ecx
+    }
+
+    return (b & (1 << 25)) != 0;
+}
+
+static void phex(uint8_t* str)
+{
+    unsigned char i;
+    for(i = 0; i < 16; ++i)
+        printf("%.2x", str[i]);
+    printf("\n");
+}
+
+static void parseInput(char *I, uint8_t *Out) {
+  int n=0;
+  const uint8_t *p;
+  const uint8_t *D;
+  uint8_t c=0, e, *d;
+  p = (const uint8_t *) I;
+  d = Out;
+  D = (d + 16);
+  for (; d != D && *p; p++) {
+      e = CHRHEX [(int) *p];
+      if (e != 0xFF) {
+          c = ((c << 4) | e);
+          n++;
+          if (n == 2) {
+              *(d++) = c;
+              n = 0;
+          }
+      }
+  }
+}
+
+__attribute__((always_inline)) static bool CompareResult(const uint8_t *A, const uint8_t *B) {
+  return !memcmp(A,B,16);
+}
+
+__attribute__((always_inline)) static void EncryptNI(uint8_t *I, const uint8_t *K) {
+  __m128i key_schedule[11];
+  aes128_load_key_enc_only(K,key_schedule);
+  aes128_enc(key_schedule,I,I);    
+}
+
+__attribute__((always_inline)) static void DecryptNI(uint8_t *I, const uint8_t *K) {  
+  __m128i key_schedule[20];
+  aes128_load_key(K,key_schedule);
+  aes128_dec(key_schedule,I,I);
+}
+
+static void parseKey(char *I, uint8_t *Out) {
+  int n=0;
+  const uint8_t *p;
+  const uint8_t *D;
+  uint8_t c=0, e, *d;
+  p = (const uint8_t *) I;
+  d = Out;
+  D = (d + 16);
+  int Index = 0;
+  for (; d != D && *p; p++) {
+      if (*p == '?') {
+        // Unknown Byte
+        BByte BB;
+        BB.Index = Index;
+        BB.Value = 0;
+        BB.Shift = MissingBytes.size() * 8;
+        MissingBytes.push_back(BB);
+
+        p++;
+        *d = 0;
+        d++;
+        n = 0;
+        Index++;
+      } else {
+        e = CHRHEX [(int) *p];
+        if (e != 0xFF) {
+            c = ((c << 4) | e);
+            n++;
+            if (n == 2) {
+                *(d++) = c;
+                n = 0;
+                Index++;
+            }
+        }
+      }      
+  }
+}
+
+static void BruteforceMissingBytes(const uint8_t Input[16], const uint8_t Expected[16], uint8_t IKey[16], bool Enc, int Round=0) {   
+  int B = 0;
+  for (auto &BB : MissingBytes) {
+    printf("[*] Byte %i ", B++);
+    printf("Index %i\n", BB.Index);        
+  }    
+
+  if (MissingBytes.size() > 6) {
+    printf("[!] Too many missing bytes! (6 max)");
+    return;
+  }
+
+  // Get amount of threads
+  uint64_t V=0;
+  uint64_t Max = (uint64_t) ((std::pow(2, MissingBytes.size() *8)) - 1);      
+
+  const int AESNI_Threads = std::thread::hardware_concurrency();
+  uint64_t Step = (Max) / AESNI_Threads;
+  uint64_t Min = 0;
+
+  printf("[*] AES-NI Units    : %i\n", AESNI_Threads);
+  printf("[*] Range           : %08lX - %08lX\n", Min, Max);  
+  printf("[*] Step            : %08lX\n", Step);
+  
+  for (int i=0;i<AESNI_Threads;i++) {
+    Range R;
+    R.Min = Min;    
+    if (Min + Step > Max) {
+      R.Max = Max;
+    } else {
+      R.Max = Min + Step;
+    }
+    
+    Min += Step + 1;
+
+    Ranges.push_back(R);
+  }
+
+  int TID = 0;
+  for (auto &R : Ranges) {
+    R.ThreadID = TID++;
+    printf("[*] T%02i Range       : %08lX - %08lX\n", R.ThreadID,R.Min,R.Max);
+  }
+ 
+  // Bruteforce threads
+  bool Finished = false;
+  std::vector<std::thread> workers;  
+  for (auto &R : Ranges) {    
+    // Encryption Thread
+    if (Enc) {
+      workers.push_back(std::thread([&]() {
+      uint8_t I[16] = {0};
+      uint8_t KeyThread[16] = {0};
+      
+      // Bruteforce
+      for (uint64_t i=R.Min; i<=R.Max; i++) {
+        if (Finished)
+          return;
+
+        // Set input key
+        memcpy(KeyThread, IKey, 16);        
+        memcpy(I, Input, 16);        
+        
+        // Set bruteforced bytes
+        for (auto &B : MissingBytes) {
+          KeyThread[B.Index] = (uint8_t) ((i >> B.Shift));
+        }        
+            
+        // Encrypt or decrcypt                    
+        EncryptNI(I, KeyThread);               
+
+        // Compare if result found
+        if (CompareResult(I, Expected) == true) {          
+            // Key found
+            Finished = true;
+
+            printf("[!] T%02i Key found    : ", R.ThreadID);
+            memcpy(IKey, KeyThread, 16);
+            phex(IKey);                          
+            return;
+          }
+      }      
+    }));
+    } else {
+      // Decryption Thread
+      workers.push_back(std::thread([&]() {
+      uint8_t I[16] = {0};
+      uint8_t KeyThread[16] = {0};
+      
+      // Bruteforce
+      for (uint64_t i=R.Min; i<=R.Max; i++) {
+        if (Finished)
+          return;
+
+        // Set input key
+        memcpy(KeyThread, IKey, 16);        
+        memcpy(I, Input, 16);        
+        
+        // Set bruteforced bytes
+        for (auto &B : MissingBytes) {
+          KeyThread[B.Index] = (uint8_t) ((i >> B.Shift));
+        }        
+            
+        // Encrypt or decrcypt    
+        // Attack Round 10 on decryption if needed
+        if (Round > 0) {                          
+          KeyExpansion(KeyThread);                                     
+        } 
+        DecryptNI(I, KeyThread);   
+
+        // Compare if result found
+        if (CompareResult(I, Expected) == true) {          
+            // Key found
+            Finished = true;
+
+            printf("[!] T%02i Key found   : ", R.ThreadID);
+            memcpy(IKey, KeyThread, 16);
+            phex(IKey);                 
+            return;
+          }
+      }      
+    }));
+    }
+  }
+
+  // Wait until all threads are finished
+  for (auto &t : workers) {        
+        t.join();                    
+    };
+}
+
+
+
+int main(int argc, char **argv) {
+  bool Enc = true;
+  int KeyScheduleRound = 0;
+  uint8_t plaintext[16] = {0};
+  uint8_t ciphertext[16] = {0};
+  uint8_t key[16] = {0};
+
+  printf("AES Key tester v0.1 (pgarba 2018)\n");
+  if (argc < 5) {
+    printf("Usage: %s mode<Enc e | Dec d> <In> <Out> <key> <keyschduleRound:opt>\n", argv[0]);
+    return 1;  
+  }
+
+  if (GetNNICapability() == false) {
+    printf("AES-NI is not supported by this CPU!\n");
+    return 0;
+  } 
+
+  printf("[*] AES-NI is supported by this CPU!\n");
+
+  if (argv[1][0] != 'e')
+    Enc = false;
+  
+  parseInput(argv[2], plaintext);
+  parseInput(argv[3], ciphertext);  
+  parseKey(argv[4], key);  
+
+   if (argc == 6) {
+    KeyScheduleRound = atoi(argv[5]);
+    printf("[*] Round           : %i\n", KeyScheduleRound);
+  }
+ 
+  printf("[*] Mode            : ");
+  if (Enc == true)
+    printf("Encryption\n");
+  else
+    printf("Decryption\n");
+
+
+  printf("[*] Key             : ");
+  phex(key);
+
+  printf("[*] Input           : ");
+  phex(plaintext);
+
+  printf("[*] Expected        : ");
+  phex(ciphertext);
+
+  if (MissingBytes.size() > 0) {
+    printf("[!] Bruteforce      : %li missing bytes\n", MissingBytes.size());
+    BruteforceMissingBytes(plaintext, ciphertext, key, Enc, KeyScheduleRound);
+  }
+
+  if (MissingBytes.size() == 0 && KeyScheduleRound > 0) {
+    KeyExpansion(key);        
+  }
+
+  if (Enc) {    
+    EncryptNI(plaintext, key);
+  } else {    
+    DecryptNI(plaintext, key);
+  }
+
+  printf("[*] Output          : ");
+  phex(plaintext);
+
+  bool R = CompareResult(plaintext, ciphertext);
+  if (R == true) {
+    printf("[!] Valid key!\n");
+  } else {
+    printf("[!] Wrong key!\n");
+  }
+
+  printf("\n");
+
+  return 0;
+}
